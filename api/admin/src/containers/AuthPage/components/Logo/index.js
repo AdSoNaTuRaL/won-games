49881dccf47299ca1701d746a50c07ae63fa5140
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-03 18:07:03 +0100

    Change logo

---
+++ api/admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoWonGames from '../../../../assets/images/logo-won-dark.svg';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoWonGames} alt="Won Games" />;
+
+export default Logo;
