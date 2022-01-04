d0673fb9f141404575036840c34245438d6665f8
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-04 19:51:25 +0100

    refactor rating element and create new service for create a game

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -15,13 +15,92 @@ async function getGameInfo(slug) {
   const body = await axios.get(`https://www.gog.com/game/${slug}`);
   const dom = new JSDOM(body.data);
 
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
   const description = dom.window.document.querySelector('.description');
 
   return {
-    rating: 'Free',
+    rating: ratingElement
+    ? ratingElement
+        .getAttribute("xlink:href")
+        .replace(/_/g, "")
+        .replace(/[^\w-]+/g, "")
+    : "FREE",
     short_description: description.textContent.trim().slice(0, 160),
     description: description.innerHTML,
   };
+}
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+  
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name: name,
+      slug: slugify(name, { lower: true }),
+    })
+  }
+}
+
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach(product => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres && genres.forEach((genre) => {
+      categories[genre] = true;
+    });
+
+    supportedOperatingSystems && supportedOperatingSystems.forEach((OS) => {
+      platforms[OS] = true;
+    });
+
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(Number(product.globalReleaseDate) * 1000).toISOString(),
+          categories: await Promise.all(product.genres.map((name) => getByName(name, "category"))),
+          platforms: await Promise.all(product.supportedOperatingSystems.map((name) => getByName(name, "platform"))),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
 }
 
 module.exports = {
@@ -32,16 +111,7 @@ module.exports = {
       data: { products },
     } = await axios.get(gogAPIURL);
 
-    console.log(products[0]);
-
-    await strapi.services.publisher.create({
-      name: products[0].publisher,
-      slug: slugify(products[0].publisher).toLowerCase(),
-    })
-
-    await strapi.services.developer.create({
-      name: products[0].developer,
-      slug: slugify(products[0].developer).toLowerCase(),
-    })
+    await createManyToManyData([products[0], products[2]]);
+    await createGames([products[4], products[5]]);
   }
 };
