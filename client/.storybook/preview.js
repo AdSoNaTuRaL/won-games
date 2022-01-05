628a18b14b757921f6528d67093daf56af254bd9
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-05 19:57:47 +0100

    refactor

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,10 +1,12 @@
-import GlobalStyles from '../src/styles/global'
+import GlobalStyles from 'styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
