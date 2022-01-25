8ae8227926704a6f60251391e7eb7a73a7096f7f
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-25 10:51:15 -0300

    add some parameters Storybook

--- client/src/components/Menu/stories.tsx
+++ client/src/components/Menu/stories.tsx
@@ -7,3 +7,10 @@ export default {
 } as Meta
 
 export const Basic: Story = (args) => <Menu {...args} />
+
+Basic.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
