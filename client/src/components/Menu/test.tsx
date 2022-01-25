ee8216dab3ba964f242d2b7b71a858a8e50a655b
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-25 10:08:40 -0300

    write first tests

--- client/src/components/Menu/test.tsx
+++ client/src/components/Menu/test.tsx
@@ -1,13 +1,25 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from './index'
 
 describe('<Menu />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Menu />)
+  it('should render menu', () => {
+    renderWithTheme(<Menu />)
 
-    expect(screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should handle open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // select menuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+    //verify if menu is hidden
+    // click on menu and check if its open
+    // click on menu and check if its closed
   })
 })
