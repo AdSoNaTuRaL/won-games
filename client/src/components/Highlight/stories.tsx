e0654edd42943f35a0cefdbc4577bb7643b3df0f
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-31 14:42:23 -0300

    add some styles and tests

--- client/src/components/Highlight/stories.tsx
+++ client/src/components/Highlight/stories.tsx
@@ -7,6 +7,7 @@ export default {
   args: {
     title: 'Red Dead is back',
     subtitle: 'Come see John new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
     buttonLabel: 'Buy now',
     buttonLink: '/rdr2'
   }
