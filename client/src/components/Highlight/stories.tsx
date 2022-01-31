55f266005cfe71bc6f790dafe4633cae77397ea8
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-31 14:21:19 -0300

    create structure and write some tests

--- client/src/components/Highlight/stories.tsx
+++ client/src/components/Highlight/stories.tsx
@@ -1,9 +1,15 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Highlight from '.'
+import Highlight, { HighlightProps } from '.'
 
 export default {
   title: 'Highlight',
-  component: Highlight
+  component: Highlight,
+  args: {
+    title: 'Red Dead is back',
+    subtitle: 'Come see John new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
 } as Meta
 
-export const Basic: Story = (args) => <Highlight {...args} />
+export const Basic: Story<HighlightProps> = (args) => <Highlight {...args} />
