7d1d3efa8fe215eced458bdf8f4f712d1c6e6842
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-31 12:18:59 -0300

    apply styles

--- client/src/components/Banner/stories.tsx
+++ client/src/components/Banner/stories.tsx
@@ -5,11 +5,14 @@ export default {
   title: 'Banner',
   component: Banner,
   args: {
-    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    img: 'https://pbs.twimg.com/media/FIvRSddXMAImXfK?format=jpg&name=medium',
     title: 'Defy death',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
     buttonLabel: 'Buy Now',
     buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
   }
 } as Meta
 
