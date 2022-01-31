22ccb4289b8653a1157a7c55ab8353e104a217bf
adsonatural <adsonhenriquesilva@gmail.com>
2022-01-31 12:45:01 -0300

    create a Ribbon component and add some tests

---
+++ client/src/components/Ribbon/styles.ts
@@ -0,0 +1,26 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import { RibbonColors, RibbonProps } from '.'
+
+const wrapperModifiers = {
+  color: (theme: DefaultTheme, color: RibbonColors) => css`
+    background-color: ${theme.colors[color]};
+  `,
+
+  normal: (theme: DefaultTheme) => css`
+    height: 3.6rem;
+    font-size: ${theme.font.sizes.small};
+  `,
+
+  small: (theme: DefaultTheme) => css`
+    height: 2.6rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `
+}
+
+export const Wrapper = styled.div<Omit<RibbonProps, 'children'>>`
+  ${({ theme, color, size }) => css`
+    ${!!size && wrapperModifiers[size](theme)}
+    ${!!color && wrapperModifiers.color(theme, color)}
+  `}
+`
