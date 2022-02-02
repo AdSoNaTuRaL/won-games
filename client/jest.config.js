652ac5879dca9bc6334162eb29409ed765eeba16
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-02 10:22:11 -0300

    add config path to jest

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
