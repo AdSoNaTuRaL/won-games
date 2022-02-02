aef22245f7e81304216eb4c8279ce467f4ab058a
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-02 10:43:31 -0300

    create a BannerSlider component and add some tests

---
+++ client/src/components/BannerSlider/index.tsx
@@ -0,0 +1,38 @@
+import Banner, { BannerProps } from 'components/Banner'
+import Slider from 'components/Slider'
+import { Settings } from 'react-slick'
+
+import * as S from './styles'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings: Settings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+  ]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default BannerSlider
