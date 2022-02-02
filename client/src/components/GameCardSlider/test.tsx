0645fc05cd85b1478b6c3852ad0aba4c6a0ec597
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-02 11:16:15 -0300

    create new tests

--- client/src/components/GameCardSlider/test.tsx
+++ client/src/components/GameCardSlider/test.tsx
@@ -1,15 +1,74 @@
+import { screen } from '@testing-library/react'
 import 'match-media-mock'
-
-import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCardSlider from './index'
 
-describe('<GameCardSlider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCardSlider />)
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
 
-    expect(
-      screen.getByRole('heading', { name: /GameCardSlider/i })
-    ).toBeInTheDocument()
+describe('<GameSlider />', () => {
+  it('should render with 4 active items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should render black arrows by default', () => {
+    renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#030517'
+    })
+
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render white arrows if color is passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
   })
 })
