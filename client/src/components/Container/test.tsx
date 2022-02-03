6afe4973934272df26521a9fcc46188f2fd37744
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-03 11:46:46 -0300

    create container component just for styles and tests

---
+++ client/src/components/Container/test.tsx
@@ -0,0 +1,37 @@
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Container } from './index'
+
+describe('<Container />', () => {
+  it('should render the container', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Adson Henrique</span>
+      </Container>
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    )
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      >
+        <span>
+          Adson Henrique
+        </span>
+      </div>
+    `)
+  })
+})
