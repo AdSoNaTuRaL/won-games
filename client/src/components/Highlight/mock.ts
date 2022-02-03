fe1d3f1c9056c326f2453c7542bcbde45fa3bce2
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-03 13:34:29 -0300

    create mock data and refactor stories files

---
+++ client/src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Red Dead is back',
+  subtitle: 'Come see John new adventures',
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
+}
--- client/src/components/Highlight/stories.tsx
+++ client/src/components/Highlight/stories.tsx
@@ -1,16 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
+import item from './mock'
 
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Red Dead is back',
-    subtitle: 'Come see John new adventures',
-    backgroundImage: '/img/red-dead-img.jpg',
-    buttonLabel: 'Buy now',
-    buttonLink: '/rdr2'
-  }
+  args: { ...item }
 } as Meta
 
 export const Basic: Story<HighlightProps> = (args) => (
