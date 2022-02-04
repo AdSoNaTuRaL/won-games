bae2552bc7fce3a7e530068d91b68c019b2ea7cc
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-04 13:25:45 -0300

    format code and fixing some global styles

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -18,8 +18,8 @@ const GlobalStyles: GlobalStyleComponent<
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
 
   @font-face {
@@ -27,8 +27,8 @@ const GlobalStyles: GlobalStyleComponent<
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
   }
 
   @font-face {
@@ -36,8 +36,8 @@ const GlobalStyles: GlobalStyleComponent<
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
   }
 
   * {
@@ -68,7 +68,6 @@ const GlobalStyles: GlobalStyleComponent<
       `}
     }
   `}
-
 `
 
 export default GlobalStyles
--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -18,19 +18,21 @@ export default {
       medium: '1.6rem',
       large: '1.8rem',
       xlarge: '2.0rem',
-      xxlarge: '2.8rem'
+      xxlarge: '2.8rem',
+      huge: '5.2rem'
     }
   },
   colors: {
     primary: '#F231A5',
     secondary: '#3CD3C1',
     mainBg: '#06092B',
-    white: '#FAFAFA',
     lightBg: '#F2F2F2',
+    white: '#FAFAFA',
     black: '#030517',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
-    darkGray: '#2E2F42'
+    darkGray: '#2E2F42',
+    red: '#FF6347'
   },
   spacings: {
     xxsmall: '0.8rem',
