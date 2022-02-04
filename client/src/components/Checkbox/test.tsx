85baeca2f9a0634df23a41e8809e03d37ab2124f
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-04 13:55:34 -0300

    create a test for oncheck

--- client/src/components/Checkbox/test.tsx
+++ client/src/components/Checkbox/test.tsx
@@ -1,6 +1,8 @@
-import { screen } from '@testing-library/react'
-import theme from 'styles/theme'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import { renderWithTheme } from 'utils/tests/helpers'
+import theme from 'styles/theme'
 
 import Checkbox from './index'
 
@@ -26,6 +28,20 @@ describe('<Checkbox />', () => {
 
     expect(screen.getByText(/checkbox label/i)).toHaveStyle({
       color: theme.colors.black
+    })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
     })
   })
 })
