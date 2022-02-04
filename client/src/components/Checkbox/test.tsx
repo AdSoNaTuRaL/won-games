483c630df1d677915a89bc7a773d1d11305f2e6e
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-04 13:08:53 -0300

    add new options for label component

--- client/src/components/Checkbox/test.tsx
+++ client/src/components/Checkbox/test.tsx
@@ -1,9 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Checkbox from './index'
 
 describe('<Checkbox />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Checkbox />)
+  it('should render checkbox with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="Checkbox Label" labelFor="checkbox" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
   })
 })
