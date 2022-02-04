76cb687c9132daed6dc20bf1aa7813436f48a521
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-04 13:55:17 -0300

    add rest of properties of input and create a oncheck function

--- client/src/components/Checkbox/index.tsx
+++ client/src/components/Checkbox/index.tsx
@@ -1,24 +1,44 @@
+import { InputHTMLAttributes, useState } from 'react'
 import * as S from './styles'
 
 export type CheckBoxProps = {
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+  onCheck?: (status: boolean) => void
+} & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckBoxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
-    {!!label && (
-      <S.Label htmlFor={labelFor} labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  labelColor = 'white',
+  onCheck
+}: CheckBoxProps) => {
+  const [checked, setChecked] = useState(false)
 
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    if (onCheck) {
+      onCheck(status)
+    }
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
 export default Checkbox
