50669979716bbb07526c14682eb869d44c9258bb
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-08 12:08:43 -0300

    apply styles

--- client/src/templates/Auth/index.tsx
+++ client/src/templates/Auth/index.tsx
@@ -10,25 +10,31 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
+      <S.BannerContent>
+        <Logo />
 
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform
-      </S.Subtitle>
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform
+          </S.Subtitle>
+        </div>
 
-      <S.Footer>
-        Won Games {new Date().getFullYear()} © All rights reserved.
-      </S.Footer>
+        <S.Footer>
+          Won Games {new Date().getFullYear()} © All rights reserved.
+        </S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
 
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
 
-      {children}
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 )
