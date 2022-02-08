4b339f6513b2cba881fdf301756e06d43ceaf5d9
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-08 14:23:21 -0300

    put links

--- client/src/templates/Auth/index.tsx
+++ client/src/templates/Auth/index.tsx
@@ -1,5 +1,6 @@
 import Heading from 'components/Heading'
 import Logo from 'components/Logo'
+import Link from 'next/link'
 import * as S from './styles'
 
 type AuthProps = {
@@ -11,7 +12,11 @@ const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
       <S.BannerContent>
-        <Logo id="banner" />
+        <Link href="/">
+          <a>
+            <Logo id="banner" />
+          </a>
+        </Link>
 
         <div>
           <Heading size="huge">All your favorite games in one place</Heading>
@@ -28,7 +33,11 @@ const Auth = ({ title, children }: AuthProps) => (
 
     <S.Content>
       <S.ContentWrapper>
-        <Logo id="content" color="black" size="large" />
+        <Link href="/">
+          <a>
+            <Logo id="content" color="black" size="large" />
+          </a>
+        </Link>
         <Heading color="black" lineColor="secondary" lineLeft>
           {title}
         </Heading>
