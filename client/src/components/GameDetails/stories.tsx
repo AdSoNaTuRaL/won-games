9e920f46692a02976a0c95cbc1c7a4681bb79ea6
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-09 13:38:41 -0300

    refactor stories

--- client/src/components/GameDetails/stories.tsx
+++ client/src/components/GameDetails/stories.tsx
@@ -8,6 +8,17 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  args: {
+    platforms: ['windows', 'linux', 'mac']
+  },
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['windows', 'linux', 'mac']
+      }
+    }
   }
 } as Meta
 
@@ -16,7 +27,3 @@ export const Basic: Story<GameDetailsProps> = (args) => (
     <GameDetails {...args} />
   </div>
 )
-
-Basic.args = {
-  platforms: ['windows', 'linux', 'mac']
-}
