60c78c6cd31c60e34ce2b259760e4c41189e5399
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-09 12:08:06 -0300

    update jest config for ignore some folders and files

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/types/**/*.ts',
+    '!src/components/**/mock.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleDirectories: ['node_modules', 'src'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
