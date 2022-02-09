50971c977177ebe69f936e0ed6465e69b948393d
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-09 12:19:31 -0300

    create a Base template for footer and Menu

---
+++ client/src/templates/Base/test.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Base from './index'
+
+jest.mock('components/Menu', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock menu"></div>
+    }
+  }
+})
+
+jest.mock('components/Footer', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock footer"></div>
+    }
+  }
+})
+
+describe('<Base />', () => {
+  it('should render the menu, footer and children', () => {
+    renderWithTheme(
+      <Base>
+        <h1>Heading</h1>
+      </Base>
+    )
+
+    expect(screen.getByTestId('Mock menu')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock footer')).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /heading/i })
+    ).toBeInTheDocument()
+  })
+})
