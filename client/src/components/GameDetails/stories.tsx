7fe7202da5675786e0c0db1cf7aba9afba5cad9c
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-09 13:34:34 -0300

    configure stories

--- client/src/components/GameDetails/stories.tsx
+++ client/src/components/GameDetails/stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
 
 export default {
   title: 'Game/GameDetails',
@@ -11,8 +11,12 @@ export default {
   }
 } as Meta
 
-export const Basic: Story = (args) => (
+export const Basic: Story<GameDetailsProps> = (args) => (
   <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
     <GameDetails {...args} />
   </div>
 )
+
+Basic.args = {
+  platforms: ['windows', 'linux', 'mac']
+}
