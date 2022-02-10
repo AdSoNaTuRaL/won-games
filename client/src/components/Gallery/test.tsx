ff116c7912ecb61849cebe9797eacf0b94b3d65d
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-10 12:56:58 -0300

    write tests for modal

--- client/src/components/Gallery/test.tsx
+++ client/src/components/Gallery/test.tsx
@@ -1,10 +1,37 @@
-import { screen } from '@testing-library/react'
+import 'match-media-mock'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Gallery from './index'
 
+import mockItems from './mock'
+
 describe('<Gallery />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Gallery />)
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery image 1/i })
+    ).toHaveAttribute('src', mockItems[0].src)
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery image 2/i })
+    ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('should handle open modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    //verify if modal is hidden
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+
+    // click on modal and check if its open
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
   })
 })
