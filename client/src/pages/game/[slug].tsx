f3f1992774e1ff0847053a3c8d2a7411386faab4
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-10 17:34:32 -0300

    add gallery to props page

--- client/src/pages/game/[slug].tsx
+++ client/src/pages/game/[slug].tsx
@@ -1,5 +1,7 @@
 import Game, { GameTemplateProps } from 'templates/Game'
 
+import galleryMock from 'components/Gallery/mock'
+
 export default function Index(props: GameTemplateProps) {
   return <Game {...props} />
 }
@@ -21,7 +23,8 @@ export async function getStaticProps() {
         price: '259,00',
         description:
           'Cyberpunk 2077 é um jogo eletrônico de RPG de ação desenvolvido e publicado pela CD Projekt. Lançado em 10 de dezembro de 2020 para Google Stadia, Microsoft Windows, PlayStation 4 e Xbox One, e previsto para 2021 para PlayStation 5 e Xbox Series X/S, sendo que nesses já está disponível via retrocompatibilidade'
-      }
+      },
+      gallery: galleryMock
     }
   }
 }
