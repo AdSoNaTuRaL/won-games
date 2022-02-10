e328f53e34eb6624dd2f239e4a138a3ed397028e
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-10 15:01:43 -0300

    add more props

--- client/src/pages/game/[slug].tsx
+++ client/src/pages/game/[slug].tsx
@@ -14,7 +14,14 @@ export async function getStaticPaths() {
 export async function getStaticProps() {
   return {
     props: {
-      cover: 'https://pbs.twimg.com/media/FHYAdEJXwAI0E78?format=jpg&name=large'
+      cover:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSET2LEixlKz6Yi9gWXwgC_5FdnYJw7RUhfV9IfpOdX7JnvG1HNUqPtGjVvH2dz98a2pYY&usqp=CAU',
+      gameInfo: {
+        title: 'Cyberpunk 2077',
+        price: '259,00',
+        description:
+          'Cyberpunk 2077 é um jogo eletrônico de RPG de ação desenvolvido e publicado pela CD Projekt. Lançado em 10 de dezembro de 2020 para Google Stadia, Microsoft Windows, PlayStation 4 e Xbox One, e previsto para 2021 para PlayStation 5 e Xbox Series X/S, sendo que nesses já está disponível via retrocompatibilidade'
+      }
     }
   }
 }
