df8b537a5d5fa7446d3e0be0656a4abdb659f20a
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-10 17:43:18 -0300

    add TextContent component to Game Page

--- client/src/templates/Game/index.tsx
+++ client/src/templates/Game/index.tsx
@@ -1,5 +1,6 @@
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
+import TextContent from 'components/TextContent'
 import Base from 'templates/Base'
 
 import * as S from './styles'
@@ -8,9 +9,10 @@ export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
+  description: string
 }
 
-const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
 
@@ -22,6 +24,10 @@ const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
       <S.SectionGallery>
         {!!gallery && <Gallery items={gallery} />}
       </S.SectionGallery>
+
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
     </S.Main>
   </Base>
 )
--- client/src/templates/Game/styles.ts
+++ client/src/templates/Game/styles.ts
@@ -53,3 +53,13 @@ export const SectionGallery = styled(Section)`
     display: block;
   `}
 `
+
+export const SectionDescription = styled(Section)`
+  ${({ theme }) => css`
+    .description__copyrights {
+      font-size: ${theme.font.sizes.xsmall};
+      margin-top: ${theme.spacings.medium};
+      color: ${theme.colors.gray};
+    }
+  `}
+`
