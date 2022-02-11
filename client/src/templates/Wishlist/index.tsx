508382d191ea430bce21ceeae5ed739857e58544
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-11 11:35:19 -0300

    use a grid component

--- client/src/templates/Wishlist/index.tsx
+++ client/src/templates/Wishlist/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'components/Container'
 import GameCard, { GameCardProps } from 'components/GameCard'
+import { Grid } from 'components/Grid'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
@@ -24,9 +25,11 @@ const Wishlist = ({
         Wishlist
       </Heading>
 
-      {games?.map((game, index) => (
-        <GameCard key={`wishlist-${index}`} {...game} />
-      ))}
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={`wishlist-${index}`} {...game} />
+        ))}
+      </Grid>
     </Container>
 
     <Showcase
