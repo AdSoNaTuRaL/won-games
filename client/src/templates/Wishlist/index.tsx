ef2bbfad345ce617ffab91a4c58caa78e8a19507
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-11 11:09:06 -0300

    create a Wishlist template

---
+++ client/src/templates/Wishlist/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+import Base from 'templates/Base'
+import * as S from './styles'
+
+const Wishlist = () => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+    </Container>
+  </Base>
+)
+
+export default Wishlist
