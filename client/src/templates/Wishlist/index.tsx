f25cc45bdf215730c5ca9d46751267044cb36974
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-11 11:16:15 -0300

    add props and initial data to template

--- client/src/templates/Wishlist/index.tsx
+++ client/src/templates/Wishlist/index.tsx
@@ -1,15 +1,32 @@
 import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
 import Heading from 'components/Heading'
+import { HighlightProps } from 'components/Highlight'
+import Showcase from 'components/Showcase'
 import Base from 'templates/Base'
+
 import * as S from './styles'
 
-const Wishlist = () => (
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 )
 
