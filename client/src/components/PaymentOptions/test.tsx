68679a99ef6b8e7f44d9fb631f37aa36c8ae086a
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-15 14:20:38 -0300

    disable button and cover with tests

--- client/src/components/PaymentOptions/test.tsx
+++ client/src/components/PaymentOptions/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import PaymentOptions from './index'
@@ -14,5 +15,40 @@ describe('<PaymentOptions />', () => {
     expect(screen.getByLabelText(/5498/)).toBeInTheDocument()
     expect(screen.getByLabelText(/5018/)).toBeInTheDocument()
     expect(screen.getByText(/add a new credit card/i)).toBeInTheDocument()
+  })
+
+  it('should handle select card when clicking on the label', async () => {
+    renderWithTheme(
+      <PaymentOptions cards={cardsMock} handlePayment={jest.fn} />
+    )
+
+    userEvent.click(screen.getByLabelText(/5498/))
+    await waitFor(() => {
+      expect(screen.getByRole('radio', { name: /5498/ })).toBeChecked()
+    })
+  })
+
+  it('should not call handlePayment when button is disabled', () => {
+    const handlePayment = jest.fn()
+    renderWithTheme(
+      <PaymentOptions cards={cardsMock} handlePayment={handlePayment} />
+    )
+
+    userEvent.click(screen.getByRole('button', { name: /buy now/i }))
+    expect(handlePayment).not.toHaveBeenCalled()
+  })
+
+  it('should call handlePayment when credit card is selected and button is enabled', async () => {
+    const handlePayment = jest.fn()
+    renderWithTheme(
+      <PaymentOptions cards={cardsMock} handlePayment={handlePayment} />
+    )
+
+    userEvent.click(screen.getByLabelText(/5498/))
+    userEvent.click(screen.getByRole('button', { name: /buy now/i }))
+
+    await waitFor(() => {
+      expect(handlePayment).toHaveBeenCalled()
+    })
   })
 })
