3bffee4a9c1112a94574b271cb6f88e08dec8cd5
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-15 13:25:24 -0300

    create CartList component and start initial configurations

---
+++ client/src/components/CartList/mock.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 3',
+    price: 'R$ 315,00'
+  }
+]
---
+++ client/src/components/CartList/test.tsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import CartList from './index'
+import mockItems from './mock'
+
+const props = {
+  items: mockItems,
+  total: 'R$ 330,00'
+}
+
+describe('<CartList />', () => {
+  it('should render the cart list', () => {
+    renderWithTheme(<CartList {...props} />)
+
+    expect(screen.getAllByRole('heading')).toHaveLength(2)
+    expect(screen.getByText(props.total)).toBeInTheDocument()
+  })
+})
