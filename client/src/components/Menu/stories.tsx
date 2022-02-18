ebf4f571e615f84e5e5a8d2ef0dd35a171fb24ab
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-18 12:29:06 -0300

    add links and dropdown to Menu component

--- client/src/components/Menu/stories.tsx
+++ client/src/components/Menu/stories.tsx
@@ -3,14 +3,19 @@ import Menu from '.'
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
 export const Basic: Story = (args) => <Menu {...args} />
 
-Basic.parameters = {
-  layout: 'fullscreen',
-  backgrounds: {
-    default: 'won-dark'
-  }
+export const Logged: Story = (args) => <Menu {...args} />
+
+Logged.args = {
+  username: 'adsonatural'
 }
