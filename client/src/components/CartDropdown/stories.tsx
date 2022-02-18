0b38b58f358c90ac25c043a20c0a7f015cae7c59
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-18 11:41:32 -0300

    create new storie for empty state and refactor props to be optional

--- client/src/components/CartDropdown/stories.tsx
+++ client/src/components/CartDropdown/stories.tsx
@@ -22,3 +22,9 @@ export const Basic: Story<CartDropdownProps> = (args) => (
     <CartDropdown {...args} />
   </div>
 )
+
+export const WithEmptyState: Story<CartDropdownProps> = () => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown />
+  </div>
+)
