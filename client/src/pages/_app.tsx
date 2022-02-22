ed4cbb1b817c5a9520ac30cb69dc237fc7aca363
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-22 11:01:31 -0300

    use useApollo hook from utils to refactor apollo provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,6 @@
-import {
-  ApolloProvider,
-  ApolloClient,
-  gql,
-  InMemoryCache
-} from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
+import { useApollo } from 'utils/apollo'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
@@ -12,10 +8,7 @@ import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
-    cache: new InMemoryCache()
-  })
+  const client = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={client}>
