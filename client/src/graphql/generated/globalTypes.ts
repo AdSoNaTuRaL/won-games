5c2d2f60647e2ba9fb9f9459f093b2609133290e
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-22 13:14:49 -0300

    auto generated by apollo codegen

--- client/src/graphql/generated/globalTypes.ts
+++ client/src/graphql/generated/globalTypes.ts
@@ -7,6 +7,15 @@
 // START Enums and Input Objects
 //==============================================================
 
+export enum ENUM_GAME_RATING {
+  FREE = "FREE",
+  pegi12 = "pegi12",
+  pegi16 = "pegi16",
+  pegi18 = "pegi18",
+  pegi3 = "pegi3",
+  pegi7 = "pegi7",
+}
+
 //==============================================================
 // END Enums and Input Objects
 //==============================================================
