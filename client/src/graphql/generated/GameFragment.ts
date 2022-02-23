366d1548f30a4c38e858476f709e63c133d0fb25
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-23 12:53:19 -0300

    autogenerated by apollo codegen

--- client/src/graphql/generated/GameFragment.ts
+++ client/src/graphql/generated/GameFragment.ts
@@ -4,32 +4,24 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL query operation: QueryGames
+// GraphQL fragment: GameFragment
 // ====================================================
 
-export interface QueryGames_games_developers {
+export interface GameFragment_developers {
   __typename: "Developer";
   name: string;
 }
 
-export interface QueryGames_games_cover {
+export interface GameFragment_cover {
   __typename: "UploadFile";
   url: string;
 }
 
-export interface QueryGames_games {
+export interface GameFragment {
   __typename: "Game";
   name: string;
   slug: string;
-  developers: QueryGames_games_developers[];
-  cover: QueryGames_games_cover | null;
+  developers: GameFragment_developers[];
+  cover: GameFragment_cover | null;
   price: number;
-}
-
-export interface QueryGames {
-  games: QueryGames_games[];
-}
-
-export interface QueryGamesVariables {
-  limit?: number | null;
 }
--- client/src/graphql/generated/QueryHome.ts
+++ client/src/graphql/generated/QueryHome.ts
@@ -36,6 +36,26 @@ export interface QueryHome_banners {
   ribbon: QueryHome_banners_ribbon | null;
 }
 
+export interface QueryHome_newGames_developers {
+  __typename: "Developer";
+  name: string;
+}
+
+export interface QueryHome_newGames_cover {
+  __typename: "UploadFile";
+  url: string;
+}
+
+export interface QueryHome_newGames {
+  __typename: "Game";
+  name: string;
+  slug: string;
+  developers: QueryHome_newGames_developers[];
+  cover: QueryHome_newGames_cover | null;
+  price: number;
+}
+
 export interface QueryHome {
   banners: QueryHome_banners[];
+  newGames: QueryHome_newGames[];
 }
