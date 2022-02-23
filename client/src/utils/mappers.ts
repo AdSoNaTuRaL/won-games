598f5344f996bf3b33f1b88d777ae7e5aed541a8
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-23 13:49:16 -0300

    create utils to map

---
+++ client/src/utils/mappers.ts
@@ -0,0 +1,47 @@
+import { QueryGames_games } from 'graphql/generated/QueryGames'
+import {
+  QueryHome_banners,
+  QueryHome_sections_freeGames_highlight
+} from 'graphql/generated/QueryHome'
+
+export const bannerMapper = (banners: QueryHome_banners[]) => {
+  return banners.map((banner) => ({
+    img: `http://localhost:1337${banner.image?.url}`,
+    title: banner.title,
+    subtitle: banner.subtitle,
+    buttonLabel: banner.button?.label,
+    buttonLink: banner.button?.link,
+    ribbon: banner.ribbon?.text || null,
+    ribbonColor: banner.ribbon?.color || null,
+    ribbonSize: banner.ribbon?.size || null
+  }))
+}
+
+export const gamesMapper = (games: QueryGames_games[] | null | undefined) => {
+  return (
+    games &&
+    games.map((game) => ({
+      slug: game.slug,
+      title: game.name,
+      developer: game.developers[0].name,
+      img: `http://localhost:1337${game.cover?.url}`,
+      price: game.price
+    }))
+  )
+}
+
+export const highlightMapper = (
+  highlight: QueryHome_sections_freeGames_highlight | null | undefined
+) => {
+  return (
+    highlight && {
+      title: highlight.title,
+      subtitle: highlight.subtitle,
+      backgroundImage: `http://localhost:1337${highlight.backgroundImage?.url}`,
+      floatImage: `http://localhost:1337${highlight.floatImage?.url}`,
+      buttonLabel: highlight.buttonLabel,
+      buttonLink: highlight.buttonLink,
+      alignment: highlight.alignment
+    }
+  )
+}
