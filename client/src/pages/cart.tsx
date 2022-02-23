83927f7e47b507c0e81a94e0ed1d09b89ac892bd
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-23 19:10:05 -0300

    refactor cart page using recommended data from recommended query

--- client/src/pages/cart.tsx
+++ client/src/pages/cart.tsx
@@ -2,21 +2,33 @@ import Cart, { CartProps } from 'templates/Cart'
 
 import itemsMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
+
+import { initializeApollo } from 'utils/apollo'
+import { queryRecommended } from 'graphql/generated/queryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<queryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
+      recommendedTitle: data.recommended?.section?.title,
       items: itemsMock,
       total: 'R$ 430, 00',
       cards: cardsMock,
-      recommendedGames: gamesMock,
-      recommendedHighlight: highlightMock
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      )
     }
   }
 }
