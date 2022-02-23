806b137627c007295ec6a55500bb02383a872495
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-23 11:18:00 -0300

    autogenerate by apollo codegen

--- client/src/graphql/generated/globalTypes.ts
+++ client/src/graphql/generated/globalTypes.ts
@@ -7,6 +7,16 @@
 // START Enums and Input Objects
 //==============================================================
 
+export enum ENUM_COMPONENTPAGERIBBON_COLOR {
+  primary = "primary",
+  secondary = "secondary",
+}
+
+export enum ENUM_COMPONENTPAGERIBBON_SIZE {
+  normal = "normal",
+  small = "small",
+}
+
 export enum ENUM_GAME_RATING {
   FREE = "FREE",
   pegi12 = "pegi12",
