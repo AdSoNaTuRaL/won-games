10d73baf497eac1d1de015325e94492a0b77ba84
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-23 13:07:06 -0300

    add two new queries: upcoming games and free games

--- client/src/graphql/queries/home.ts
+++ client/src/graphql/queries/home.ts
@@ -15,6 +15,18 @@ export const QUERY_HOME = gql`
     ) {
       ...GameFragment
     }
+
+    upcomingGames: games(
+      where: { release_date_gt: "2021-09-01" }
+      sort: "release_date:asc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
+
+    freeGames: games(where: { price: 0 }, sort: "release_date:desc", limit: 8) {
+      ...GameFragment
+    }
   }
 
   ${BannerFragment}
