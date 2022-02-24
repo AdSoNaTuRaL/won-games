65ab9f260e085dff76ed842b8b6f7164a2bc99d6
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-24 11:05:38 -0300

    use custom hook and format code

--- client/src/templates/Games/index.tsx
+++ client/src/templates/Games/index.tsx
@@ -1,14 +1,13 @@
-import { useQuery } from '@apollo/client'
 import { KeyboardArrowDown } from 'styled-icons/material-outlined'
 import { Spinner } from 'styled-icons/evil'
 
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
+
 import Base from 'templates/Base'
 
-import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
-import { QUERY_GAMES } from 'graphql/queries/games'
+import { useQueryGames } from 'graphql/queries/games'
 
 import * as S from './styles'
 
@@ -18,10 +17,10 @@ export type GamesTemplateProps = {
 }
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
-  const { data, loading, fetchMore } = useQuery<
-    QueryGames,
-    QueryGamesVariables
-  >(QUERY_GAMES, { variables: { limit: 15, start: 0 } })
+  const { data, loading, fetchMore } = useQueryGames({
+    variables: { limit: 15 }
+  })
+
   const handleFilter = () => {
     return
   }
