c054f9a64417db04ab3afa7a547a93dc95cc9611
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-28 19:48:50 -0300

    fix: revalidate props and fetchPolicy from apollo to no cache

--- client/src/pages/game/[slug].tsx
+++ client/src/pages/game/[slug].tsx
@@ -48,7 +48,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { data } = await apolloClient.query<
     QueryGameBySlug,
     QueryGameBySlugVariables
-  >({ query: QUERY_GAME_BY_SLUG, variables: { slug: `${params?.slug}` } })
+  >({
+    query: QUERY_GAME_BY_SLUG,
+    variables: { slug: `${params?.slug}` },
+    fetchPolicy: 'no-cache'
+  })
 
   if (!data.games.length) {
     return { notFound: true }
@@ -72,8 +76,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   })
 
   return {
+    revalidate: 60,
     props: {
-      revalidate: 60,
       cover: `http://localhost:1337${game.cover?.src}`,
       gameInfo: {
         title: game.name,
