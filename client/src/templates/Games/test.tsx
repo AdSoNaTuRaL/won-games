8736a9dbcadbae73c933bf4bb25f274c6f6472a8
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-28 14:09:37 -0300

    refactor: fix tests and add more

--- client/src/templates/Games/test.tsx
+++ client/src/templates/Games/test.tsx
@@ -11,6 +11,17 @@ import { fetchMoreMock, gamesMock } from './mocks'
 
 import Games from './index'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
+
 jest.mock('templates/Base', () => ({
   __esModule: true,
   default: function Mock({ children }: { children: React.ReactNode }) {
@@ -18,10 +29,10 @@ jest.mock('templates/Base', () => ({
   }
 }))
 
-jest.mock('components/ExploreSidebar', () => ({
+jest.mock('next/link', () => ({
   __esModule: true,
   default: function Mock({ children }: { children: React.ReactNode }) {
-    return <div data-testid="Mock ExploreSidebar">{children}</div>
+    return <div>{children}</div>
   }
 }))
 
@@ -47,7 +58,7 @@ describe('<Games />', () => {
     expect(screen.getByRole('img', { name: /loading.../i })).toBeInTheDocument()
 
     // wait to have data and then get elements - find => asyncron proccess
-    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
+    expect(await screen.findByText(/price/i)).toBeInTheDocument()
 
     expect(await screen.findByText(/sample game/i)).toBeInTheDocument()
 
@@ -68,5 +79,21 @@ describe('<Games />', () => {
     userEvent.click(await screen.findByRole('button', { name: /show more/i }))
 
     expect(await screen.findByText(/fetch more game/i)).toBeInTheDocument()
+  })
+
+  it('should change push router when selecting a filter', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
+    userEvent.click(await screen.findByRole('radio', { name: /low to high/i }))
+
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/games',
+      query: { platforms: ['windows'], sort_by: 'low-to-high' }
+    })
   })
 })
