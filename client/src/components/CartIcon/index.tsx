7bff4cdb8da22bb524ea9779b39157ba209fc67e
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-02 14:17:19 -0300

    refactor: delete quantity props, retrieve quantity from usecart (context api)

--- client/src/components/CartIcon/index.tsx
+++ client/src/components/CartIcon/index.tsx
@@ -1,15 +1,14 @@
+import { useCart } from 'hooks/use-cart'
 import { ShoppingCart } from 'styled-icons/material-outlined'
 import * as S from './styles'
 
-export type CartIconProps = {
-  quantity?: number
+const CartIcon = () => {
+  const { quantity } = useCart()
+  return (
+    <S.Wrapper>
+      {quantity > 0 && <S.Badge aria-label="card items">{quantity}</S.Badge>}
+      <ShoppingCart aria-label="Shopping cart" />
+    </S.Wrapper>
+  )
 }
-
-const CartIcon = ({ quantity = 0 }: CartIconProps) => (
-  <S.Wrapper>
-    {quantity > 0 && <S.Badge aria-label="card items">{quantity}</S.Badge>}
-    <ShoppingCart aria-label="Shopping cart" />
-  </S.Wrapper>
-)
-
 export default CartIcon
