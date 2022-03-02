9dac1b8c42b4d28c9846ef55e6419d2eca8e89b8
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-02 15:35:02 -0300

    fix: add id

--- client/src/templates/Games/index.tsx
+++ client/src/templates/Games/index.tsx
@@ -69,6 +69,7 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
               <Grid>
                 {data?.games.map((game) => (
                   <GameCard
+                    id={game.id}
                     key={game.slug}
                     title={game.name}
                     slug={game.slug}
