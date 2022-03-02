134b048c0066c1f2a6f8c25ceb60a587b5539c23
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-02 12:26:38 -0300

    refactor

--- client/src/hooks/use-cart/index.tsx
+++ client/src/hooks/use-cart/index.tsx
@@ -1,10 +1,19 @@
+import { useQueryGames } from 'graphql/queries/games'
 import { createContext, useContext, useState, useEffect } from 'react'
 import { getStorageItem } from 'utils/localStorage'
+import { cartMapper } from 'utils/mappers'
 
 const CART_KEY = 'cartItems'
 
+type CartItem = {
+  id: string
+  img: string
+  title: string
+  price: string
+}
+
 export type CartContextData = {
-  items: string[]
+  items: CartItem[]
 }
 
 export const CartContextDefaultValues = {
@@ -30,10 +39,17 @@ const CartProvider = ({ children }: CartProviderProps) => {
     }
   }, [])
 
+  const { data } = useQueryGames({
+    skip: !cartItems.length,
+    variables: {
+      where: { id: cartItems }
+    }
+  })
+
   return (
     <CartContext.Provider
       value={{
-        items: cartItems
+        items: cartMapper(data?.games)
       }}
     >
       {children}
