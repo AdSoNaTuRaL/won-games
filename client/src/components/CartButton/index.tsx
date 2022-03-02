4b99a572958c884b4be84344f78d7252c08d6aa1
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-02 15:01:07 -0300

    create a cart button component

---
+++ client/src/components/CartButton/index.tsx
@@ -0,0 +1,29 @@
+import Button from 'components/Button'
+import { useCart } from 'hooks/use-cart'
+import {
+  AddShoppingCart,
+  RemoveShoppingCart
+} from 'styled-icons/material-outlined'
+
+type CartButtonProps = {
+  id: string
+}
+
+const CartButton = ({ id }: CartButtonProps) => {
+  const { isInCart, addToCart, removeFromCart } = useCart()
+  return (
+    <Button
+      icon={
+        isInCart(id) ? (
+          <RemoveShoppingCart aria-label="Remove from cart" />
+        ) : (
+          <AddShoppingCart aria-label="Add to cart" />
+        )
+      }
+      size="small"
+      onClick={() => (isInCart(id) ? removeFromCart(id) : addToCart(id))}
+    />
+  )
+}
+
+export default CartButton
