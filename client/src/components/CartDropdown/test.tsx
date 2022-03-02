ca7147592b39c7e1e007064ebae5e2223e79d0cc
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-02 14:46:56 -0300

    refactor: use useCard hook (context api)

--- client/src/components/CartDropdown/test.tsx
+++ client/src/components/CartDropdown/test.tsx
@@ -3,18 +3,25 @@ import { screen, render } from 'utils/test-utils'
 import CartDropdown from './index'
 
 import itemsMock from 'components/CartList/mock'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 
 describe('<CartDropdown />', () => {
-  it('should render <CartIcon /> and its badge', () => {
-    render(<CartDropdown items={itemsMock} total="R$ 300,00" />)
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items: itemsMock,
+      quantity: itemsMock.length,
+      total: 'R$ 300,00'
+    }
+    render(<CartDropdown />, { cartProviderProps })
+  })
 
+  it('should render <CartIcon /> and its badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${itemsMock.length}`)).toBeInTheDocument()
   })
 
   it('should render dropdown content with cart items and total', () => {
-    render(<CartDropdown items={itemsMock} total="R$ 300,00" />)
-
     expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
     expect(screen.getByText('Red Dead Redemption 3')).toBeInTheDocument()
   })
