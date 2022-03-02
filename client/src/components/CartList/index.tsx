6258024db95462e1eacde5800f664253860289b9
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-02 15:45:58 -0300

    refactor: use loader component to show while the games is loading in the cart

--- client/src/components/CartList/index.tsx
+++ client/src/components/CartList/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Button from 'components/Button'
 import Empty from 'components/Empty'
 import GameItem from 'components/GameItem'
+import Loader from 'components/Loader'
 
 import * as S from './styles'
 
@@ -12,7 +13,16 @@ export type CartListProps = {
 }
 
 const CartList = ({ hasButton = false }: CartListProps) => {
-  const { items, total } = useCart()
+  const { items, total, loading } = useCart()
+
+  if (loading) {
+    return (
+      <S.Loading>
+        <Loader />
+      </S.Loading>
+    )
+  }
+
   return (
     <S.Wrapper isEmpty={!items.length}>
       {items.length ? (
