07f9a4766365a125ee1c9eef35a0f6d42f597150
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-03 15:26:08 -0300

    wrapping all components with session provider from next-auth

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ApolloProvider } from '@apollo/client'
+import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'styled-components'
 import { CartProvider } from 'hooks/use-cart'
 import { useApollo } from 'utils/apollo'
@@ -13,30 +14,32 @@ function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/logo-won.png" />
-            <link rel="apple-touch-icon" href="/img/logo-won.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="The best gamestore in the world"
+    <SessionProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/logo-won.png" />
+              <link rel="apple-touch-icon" href="/img/logo-won.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="The best gamestore in the world"
+              />
+            </Head>
+            <GlobalStyles />
+            <NextNProgress
+              color="#F231A5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={5}
             />
-          </Head>
-          <GlobalStyles />
-          <NextNProgress
-            color="#F231A5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={5}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </SessionProvider>
   )
 }
 
