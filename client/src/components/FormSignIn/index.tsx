6b041c7e9c6e553e54e6e889e61b58498204bcb7
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-03 16:03:00 -0300

    add method to sign in with next-auth sign in

--- client/src/components/FormSignIn/index.tsx
+++ client/src/components/FormSignIn/index.tsx
@@ -1,41 +1,71 @@
 import Link from 'next/link'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { signIn } from 'next-auth/react'
 import { Email, Lock } from 'styled-icons/material-outlined'
 
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-
 import { FormWrapper, FormLink } from 'components/Form/index'
+
 import * as S from './styles'
 
-const FormSignIn = () => (
-  <FormWrapper>
-    <form>
-      <TextField
-        name="email"
-        placeholder="E-mail"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="passwprd"
-        icon={<Lock />}
-      />
-      <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+const FormSignIn = () => {
+  const [values, setValues] = useState({})
+  const { push } = useRouter()
 
-      <Button size="large" fullWidth>
-        Sign in now
-      </Button>
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
 
-      <FormLink>
-        Don&apos;t have an account?
-        <Link href="/sign-up">
-          <a> Sign Up</a>
-        </Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    const result = await signIn<'credentials'>('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: '/'
+    })
+
+    if (result?.url) {
+      return push(result.url)
+    }
+
+    console.error('Email ou senha invalidos')
+  }
+
+  return (
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="email"
+          placeholder="E-mail"
+          type="email"
+          onInputChange={(value) => handleInput('identifier', value)}
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          onInputChange={(value) => handleInput('password', value)}
+          icon={<Lock />}
+        />
+        <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+
+        <Button type="submit" size="large" fullWidth>
+          Sign in now
+        </Button>
+
+        <FormLink>
+          Don&apos;t have an account?
+          <Link href="/sign-up">
+            <a> Sign Up</a>
+          </Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignIn
