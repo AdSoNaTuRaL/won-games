c630cee3b5288a3a63fc02dd1bc6d4fed688e9c4
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-04 13:21:31 -0300

    fetching data and passing to form profile

--- client/src/pages/profile/me.tsx
+++ client/src/pages/profile/me.tsx
@@ -1,23 +1,32 @@
 import { GetServerSidePropsContext } from 'next'
 
-import FormProfile from 'components/FormProfile'
+import FormProfile, { FormProfileProps } from 'components/FormProfile'
 
 import Profile from 'templates/Profile'
 
 import protectedRoutes from 'utils/protected-routes'
+import { initializeApollo } from 'utils/apollo'
 
-export default function Me() {
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
+import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
+
+export default function Me(props: FormProfileProps) {
   return (
     <Profile>
-      <FormProfile />
+      <FormProfile {...props} />
     </Profile>
   )
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<QueryProfileMe>({
+    query: QUERY_PROFILE_ME
+  })
 
   return {
-    props: { session }
+    props: { session, username: data.me?.username, email: data.me?.email }
   }
 }
