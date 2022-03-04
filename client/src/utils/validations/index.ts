ae2bac1368f3ee46515cf41a74d98b8b06865c58
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-04 11:39:31 -0300

    create utils to validate fiedls using Joi

---
+++ client/src/utils/validations/index.ts
@@ -0,0 +1,50 @@
+import Joi from 'joi'
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+
+export type FieldErrors = {
+  [key: string]: string
+}
+
+type SignValues = Omit<UsersPermissionsRegisterInput, 'username'>
+
+const fieldsValidations = {
+  username: Joi.string().min(5).required(),
+
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+
+  password: Joi.string().min(8).required(),
+
+  confirm_password: Joi.string()
+    .valid(Joi.ref('password'))
+    .required()
+    .messages({
+      'any.only': 'confirm password does not match'
+    })
+}
+
+function getFieldErrors(objectErrors: Joi.ValidationResult) {
+  const errors: FieldErrors = {}
+
+  if (objectErrors.error) {
+    objectErrors.error.details.forEach((err) => {
+      errors[err.path.join('.')] = err.message
+    })
+  }
+
+  return errors
+}
+
+export function signInValidation(values: SignValues) {
+  const { email, password } = fieldsValidations
+  const schema = Joi.object({ email, password })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
+
+export function signUpValidation(values: SignValues) {
+  const schema = Joi.object(fieldsValidations)
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
