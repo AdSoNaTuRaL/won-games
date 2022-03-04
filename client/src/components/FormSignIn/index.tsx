cec037ab1adab624b775d0d30956da4c23074190
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-04 12:34:59 -0300

    use callback urls to redirect user to desired page

--- client/src/components/FormSignIn/index.tsx
+++ client/src/components/FormSignIn/index.tsx
@@ -22,7 +22,7 @@ const FormSignIn = () => {
   const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState({ email: '', password: '' })
   const [loading, setLoading] = useState(false)
-  const { push } = useRouter()
+  const { push, query } = useRouter()
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }))
@@ -45,7 +45,7 @@ const FormSignIn = () => {
     const result = await signIn<'credentials'>('credentials', {
       ...values,
       redirect: false,
-      callbackUrl: '/'
+      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
     })
 
     if (result?.url) {
