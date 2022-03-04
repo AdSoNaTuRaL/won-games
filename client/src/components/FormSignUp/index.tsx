4c834fce446118720e73e3574207377c2668bb2a
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-04 12:08:29 -0300

    add validations to sign up

--- client/src/components/FormSignUp/index.tsx
+++ client/src/components/FormSignUp/index.tsx
@@ -2,16 +2,30 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { signIn } from 'next-auth/client'
 import { useMutation } from '@apollo/client'
-import { AccountCircle, Email, Lock } from 'styled-icons/material-outlined'
+import {
+  AccountCircle,
+  Email,
+  Lock,
+  ErrorOutline
+} from 'styled-icons/material-outlined'
 
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-import { FormWrapper, FormLink, FormLoading } from 'components/Form/index'
+import {
+  FormWrapper,
+  FormLink,
+  FormLoading,
+  FormError
+} from 'components/Form/index'
 
 import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
 import { MUTATION_REGISTER } from 'graphql/mutations/register'
 
+import { FieldErrors, signUpValidation } from 'utils/validations'
+
 const FormSignUp = () => {
+  const [formError, setFormError] = useState('')
+  const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState<UsersPermissionsRegisterInput>({
     email: '',
     username: '',
@@ -19,7 +33,11 @@ const FormSignUp = () => {
   })
 
   const [createUser, { error, loading }] = useMutation(MUTATION_REGISTER, {
-    onError: (err) => console.error(err),
+    onError: (err) =>
+      setFormError(
+        err?.graphQLErrors[0]?.extensions?.exception.data.message[0].messages[0]
+          .message
+      ),
     onCompleted: () => {
       !error &&
         signIn<'credentials'>('credentials', {
@@ -36,6 +54,16 @@ const FormSignUp = () => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
+    setFormError('')
+
+    const errors = signUpValidation(values)
+
+    if (Object.keys(errors).length) {
+      setFieldError(errors)
+      return
+    }
+
+    setFieldError({})
 
     await createUser({
       variables: {
@@ -50,11 +78,17 @@ const FormSignUp = () => {
 
   return (
     <FormWrapper>
+      {!!formError && (
+        <FormError>
+          <ErrorOutline /> {formError}
+        </FormError>
+      )}
       <form onSubmit={handleSubmit}>
         <TextField
           name="username"
           type="text"
           placeholder="Username"
+          error={fieldError?.username}
           onInputChange={(value) => handleInput('username', value)}
           icon={<AccountCircle />}
         />
@@ -62,6 +96,7 @@ const FormSignUp = () => {
           name="email"
           type="email"
           placeholder="E-mail"
+          error={fieldError?.email}
           onInputChange={(value) => handleInput('email', value)}
           icon={<Email />}
         />
@@ -69,14 +104,16 @@ const FormSignUp = () => {
           name="password"
           type="password"
           placeholder="Password"
+          error={fieldError?.password}
           onInputChange={(value) => handleInput('password', value)}
           icon={<Lock />}
         />
         <TextField
-          name="confirm-password"
+          name="confirm_password"
           type="password"
           placeholder="Confirm password"
-          onInputChange={(value) => handleInput('confirm-password', value)}
+          error={fieldError?.confirm_password}
+          onInputChange={(value) => handleInput('confirm_password', value)}
           icon={<Lock />}
         />
 
