353d8fb4c64a26c07e163315ec1b664ceba7f0cb
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-07 12:44:04 -0300

    add a button on page me (profile) for reset password

--- client/src/components/FormProfile/index.tsx
+++ client/src/components/FormProfile/index.tsx
@@ -1,6 +1,7 @@
 import Button from 'components/Button'
 import Heading from 'components/Heading'
 import TextField from 'components/TextField'
+import Link from 'next/link'
 import * as S from './styles'
 
 export type FormProfileProps = {
@@ -30,20 +31,15 @@ const FormProfile = ({ email, username }: FormProfileProps) => (
         type="email"
         initialValue={email}
       />
-      <TextField
-        name="password"
-        placeholder="Type your password"
-        label="Password"
-        type="password"
-      />
-      <TextField
-        name="new_password"
-        placeholder="New password"
-        label="New password"
-        type="password"
-      />
 
-      <Button size="large">Save</Button>
+      <S.ButtonContainer>
+        <Link href={`/forgot-password?email=${email}`} passHref>
+          <Button as="a" minimal size="medium">
+            Reset password
+          </Button>
+        </Link>
+        <Button size="medium">Save</Button>
+      </S.ButtonContainer>
     </S.Form>
   </>
 )
