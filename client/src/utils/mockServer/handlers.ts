8e1913980099a4c18ae50467a9a8453b6cd882db
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-07 13:08:27 -0300

    create handler to intercept calls from forgot password

--- client/src/utils/mockServer/handlers.ts
+++ client/src/utils/mockServer/handlers.ts
@@ -1,3 +1,41 @@
 import { rest } from 'msw'
 
-export const handlers = []
+type LoginReqBody = {
+  email: string
+}
+
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body
+
+      // error
+      if (email === 'false@gmail.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'This email does not exist'
+                  }
+                ]
+              }
+            ]
+          })
+        )
+      }
+
+      // success
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true
+        })
+      )
+    }
+  )
+]
