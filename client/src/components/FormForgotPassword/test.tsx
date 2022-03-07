75c393b82665ae1e1a99efd43907756754473c76
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-07 13:54:16 -0300

    organize imports

--- client/src/components/FormForgotPassword/test.tsx
+++ client/src/components/FormForgotPassword/test.tsx
@@ -1,5 +1,5 @@
-import userEvent from '@testing-library/user-event'
 import 'server.mock'
+import userEvent from '@testing-library/user-event'
 import { render, screen } from 'utils/test-utils'
 
 import FormForgotPassword from '.'
