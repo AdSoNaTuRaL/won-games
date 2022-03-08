7c7d769d3b504b1a6b9f2184a6691c376caced84
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-08 12:24:26 -0300

    create custom controllers to wishlist entity

---
+++ api/api/wishlist/controllers/wishlist.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { sanitizeEntity } = require("strapi-utils");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async create(ctx) {
+    const token = await strapi.plugins["users-permissions"].services.jwt.getToken(ctx)
+
+    const body = {
+      ...ctx.request.body,
+      user: token.id
+    };
+
+    const entity = await strapi.services.wishlist.create(body)
+
+    return sanitizeEntity(entity, { model: strapi.models.wishlist })
+  },
+
+  async update(ctx) {
+    try {
+      const entity = await strapi.services.wishlist.update(
+        { id: ctx.params.id },
+        ctx.request.body
+      );
+
+      return sanitizeEntity(entity, { model: strapi.models.wishlist });
+    } catch (error) {
+      throw strapi.errors.unauthorized(error);
+    }
+  }
+};
