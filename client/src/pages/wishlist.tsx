12594ddd4a18dc5c884e52481b4eb2f5fe4b6c14
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-09 11:23:09 -0300

    create wishlist page using apollo cache

--- client/src/pages/wishlist.tsx
+++ client/src/pages/wishlist.tsx
@@ -6,6 +6,11 @@ import gamesMock from 'components/GameCardSlider/mock'
 
 import { queryRecommended } from 'graphql/generated/queryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import {
+  QueryWishlist,
+  QueryWishlistVariables
+} from 'graphql/generated/QueryWishlist'
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
 
 import { gamesMapper, highlightMapper } from 'utils/mappers'
 import protectedRoutes from 'utils/protected-routes'
@@ -17,8 +22,16 @@ export default function WishlistPage(props: WishlistTemplateProps) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
 
-  const apolloClient = initializeApollo()
+  if (!session) return {}
+
+  await apolloClient.query<QueryWishlist, QueryWishlistVariables>({
+    query: QUERY_WISHLIST,
+    variables: {
+      identifier: session.user?.email as string
+    }
+  })
 
   const { data } = await apolloClient.query<queryRecommended>({
     query: QUERY_RECOMMENDED
@@ -27,6 +40,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       session,
+      initialApolloState: apolloClient.cache.extract(),
       games: gamesMock,
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
