c24f266435462a7d02396b847c36595fa726b528
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-10 14:40:23 -0300

    create success page and template

---
+++ client/src/templates/Success/index.tsx
@@ -0,0 +1,51 @@
+import { Done } from '@styled-icons/material-outlined/Done'
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+import Showcase from 'components/Showcase'
+import Link from 'next/link'
+import Base from 'templates/Base'
+import * as S from './styles'
+
+export type SuccessTemplateProps = {
+  recommendedTitle: string
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Success = ({
+  recommendedTitle,
+  recommendedGames,
+  recommendedHighlight
+}: SuccessTemplateProps) => {
+  return (
+    <Base>
+      <Container>
+        <S.Wrapper>
+          <S.Heading>Your purchase was successful!</S.Heading>
+
+          <S.CheckMark>
+            <Done />
+          </S.CheckMark>
+
+          <S.Text>
+            Wait for your payment details by email. Your game is now available
+            for download inside your
+            <Link href="/profile/orders">
+              <a>Orders List</a>
+            </Link>
+            . Enjoy!
+          </S.Text>
+        </S.Wrapper>
+      </Container>
+
+      <Showcase
+        title={recommendedTitle}
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
+
+export default Success
