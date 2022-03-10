cd705dbcf26d0a9184956018afd228bdbce2c066
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-10 14:52:08 -0300

    add useeffect to clean cart

--- client/src/templates/Success/index.tsx
+++ client/src/templates/Success/index.tsx
@@ -3,7 +3,9 @@ import { Container } from 'components/Container'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
+import { useCart } from 'hooks/use-cart'
 import Link from 'next/link'
+import { useEffect } from 'react'
 import Base from 'templates/Base'
 import * as S from './styles'
 
@@ -18,6 +20,12 @@ const Success = ({
   recommendedGames,
   recommendedHighlight
 }: SuccessTemplateProps) => {
+  const { clearCart } = useCart()
+
+  useEffect(() => {
+    clearCart()
+  }, [clearCart])
+
   return (
     <Base>
       <Container>
@@ -32,7 +40,7 @@ const Success = ({
             Wait for your payment details by email. Your game is now available
             for download inside your
             <Link href="/profile/orders">
-              <a>Orders List</a>
+              <a> Orders List</a>
             </Link>
             . Enjoy!
           </S.Text>
