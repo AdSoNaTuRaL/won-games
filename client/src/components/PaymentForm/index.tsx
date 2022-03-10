344f0c1735bff4b3b0967fec41b9c17f202aa955
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-10 12:54:09 -0300

    style cardElement

--- client/src/components/PaymentForm/index.tsx
+++ client/src/components/PaymentForm/index.tsx
@@ -21,7 +21,14 @@ const PaymentForm = () => {
         </Heading>
 
         <CardElement
-          options={{ hidePostalCode: true }}
+          options={{
+            hidePostalCode: true,
+            style: {
+              base: {
+                fontSize: '16px'
+              }
+            }
+          }}
           onChange={handleChange}
         />
 
