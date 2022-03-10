62361594ca9caabd5b9f08201eb7f6d048adaef4
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-10 11:35:48 -0300

    return boolean freegames when total is 0

--- api/api/order/controllers/order.js
+++ api/api/order/controllers/order.js
@@ -33,6 +33,12 @@ module.exports = {
       return acc + game.price
     }, 0)
 
+    if (total === 0) {
+      return {
+        freeGames: true
+      }
+    }
+
     return { total_in_cents: total * 100, games };
   }
 };
