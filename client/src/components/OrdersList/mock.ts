2886b148a4e082c618d09705ec8156d5705c21c1
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-11 14:21:09 -0300

    refactor mock to match new structure

--- client/src/components/OrdersList/mock.ts
+++ client/src/components/OrdersList/mock.ts
@@ -1,28 +1,40 @@
 export default [
   {
     id: '1',
-    img: 'https://source.unsplash.com/user/willianjusten/151x70',
-    title: 'Red Dead Redemption 2',
-    price: 'R$ 215,00',
-    downloadLink: 'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
     paymentInfo: {
       flag: 'mastercard',
       img: '/img/cards/mastercard.png',
       number: '**** **** **** 4326',
       purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
-    }
+    },
+    games: [
+      {
+        id: '1',
+        title: 'game',
+        downloadLink:
+          'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+        img: 'https://source.unsplash.com/user/willianjusten/151x70',
+        price: 'R$ 215,00'
+      }
+    ]
   },
   {
     id: '2',
-    img: 'https://source.unsplash.com/user/willianjusten/151x70',
-    title: 'Red Dead Redemption 2',
-    price: 'R$ 215,00',
-    downloadLink: 'https://wongames.com/game/download/kjhejl867asd76DEh',
     paymentInfo: {
-      flag: 'visa',
-      img: '/img/cards/visa.png',
-      number: '**** **** **** 5423',
-      purchaseDate: 'Purchase made on 08/20/2020 at 21:12'
-    }
+      flag: 'mastercard',
+      img: '/img/cards/mastercard.png',
+      number: '**** **** **** 4326',
+      purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
+    },
+    games: [
+      {
+        id: '1',
+        title: 'game',
+        downloadLink:
+          'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+        img: 'https://source.unsplash.com/user/willianjusten/151x70',
+        price: 'R$ 215,00'
+      }
+    ]
   }
 ]
