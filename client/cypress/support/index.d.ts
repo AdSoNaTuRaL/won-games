290fe0f246e58a6465989dc152bd2e902ab6dc69
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-14 16:13:43 -0300

    initial config for cypress

---
+++ client/cypress/support/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to visit google page
+     * @example cy.google()
+     */
+    google(): Chainable<Window>
+  }
+}
