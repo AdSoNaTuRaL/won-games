0e0c02b7b7e96975f0e4f5029fbe6f395b7a6201
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-14 13:25:40 -0300

    improve acessibility

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -17,12 +17,12 @@ const Footer = () => (
         <a href="malito:adsonhenriquesilva@gmail.com">Email-me</a>
       </S.Column>
 
-      <S.Column>
+      <S.Column aria-labelledby="social-media">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Follow us
         </Heading>
 
-        <nav aria-labelledby="social media">
+        <nav id="social-media">
           <a
             href="https://www.instagram.com/adsonatural"
             target="_blank"
@@ -54,12 +54,12 @@ const Footer = () => (
         </nav>
       </S.Column>
 
-      <S.Column>
+      <S.Column aria-labelledby="resources">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Links
         </Heading>
 
-        <nav aria-labelledby="footer resources">
+        <nav id="resources">
           <Link href="/">
             <a>Home</a>
           </Link>
@@ -72,7 +72,7 @@ const Footer = () => (
         </nav>
       </S.Column>
 
-      <S.Column>
+      <S.Column aria-labelledby="contact">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Location
         </Heading>
