cd271eb77bb20501f01a78ca753623316510b49b
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-14 13:12:41 -0300

    config nextseo for games page

--- client/src/templates/Game/index.tsx
+++ client/src/templates/Game/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import { Divider } from 'components/Divider'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import { GameCardProps } from 'components/GameCard'
@@ -11,6 +12,7 @@ import Base from 'templates/Base'
 import * as S from './styles'
 
 export type GameTemplateProps = {
+  slug: string
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
@@ -24,6 +26,7 @@ export type GameTemplateProps = {
 }
 
 const Game = ({
+  slug,
   cover,
   gameInfo,
   gallery,
@@ -36,6 +39,22 @@ const Game = ({
   recommendedGames
 }: GameTemplateProps) => (
   <Base>
+    <NextSeo
+      title={`${gameInfo.title} - Won Games`}
+      description={gameInfo.description}
+      canonical={`https://wongames.adsonatural.com.br/game/${slug}`}
+      openGraph={{
+        url: `https://wongames.adsonatural.com.br/game/${slug}`,
+        title: `${gameInfo.title} - Won Games`,
+        description: gameInfo.description,
+        images: [
+          {
+            url: cover,
+            alt: `${gameInfo.title}`
+          }
+        ]
+      }}
+    />
     <S.Cover src={cover} role="image" aria-label="cover" />
 
     <S.Main>
