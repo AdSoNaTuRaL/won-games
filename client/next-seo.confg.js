c5c05c7908c157479c234392d6f55f27e5bd0596
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-14 13:06:09 -0300

    config next-seo

---
+++ client/next-seo.confg.js
@@ -0,0 +1,14 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.wongames.adsonatural.com.br',
+    site_name: 'Won Games',
+    title: 'Won Games'
+  },
+  twitter: {
+    handle: '@adsonatural',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,6 +5,9 @@ import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 import { Provider as AuthProvider } from 'next-auth/client'
 
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.confg'
+
 import { useApollo } from 'utils/apollo'
 
 import GlobalStyles from 'styles/global'
@@ -32,6 +35,7 @@ function App({ Component, pageProps }: AppProps) {
                   content="The best gamestore in the world"
                 />
               </Head>
+              <DefaultSeo {...SEO} />
               <GlobalStyles />
               <NextNProgress
                 color="#F231A5"
