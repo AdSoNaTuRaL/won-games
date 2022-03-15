c65e55b653baf3a1a077ee9c7b74299f86dd4e0c
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-15 13:00:31 -0300

    write new commands and its types

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -25,6 +25,10 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import '@testing-library/cypress/add-commands';
 
+Cypress.Commands.add('getByDataCy', (selector, ...args) => {
+  return cy.get(`[data-cy="${selector}"]`, ...args)
+})
+
 Cypress.Commands.add('shouldRenderBanner', () => {
   cy.get('.slick-slider').within(() => {
     cy.findByRole('heading', { name: /cyberpunk-2077/i })
@@ -35,5 +39,21 @@ Cypress.Commands.add('shouldRenderBanner', () => {
 
     cy.findByRole('heading', { name: /horizon forbidden west/i })
     cy.findByRole('link', { name: /buy now/i })
+  })
+})
+
+Cypress.Commands.add('shouldRenderShowcase', ({ name, highlight = false }) => {
+  cy.getByDataCy(`${name}`).within(() => {
+    cy.findByRole('heading', { name }).should('exist')
+
+    cy.getByDataCy('highlight').should(highlight ? 'exist' : 'not.exist')
+
+    if (highlight) {
+      cy.getByDataCy('highlight').within(() => {
+        cy.findByRole('link').should('have.attr', 'href')
+      })
+    }
+
+    cy.getByDataCy('game-card').should('have.length.gte', 0)
   })
 })
--- client/cypress/support/index.d.ts
+++ client/cypress/support/index.d.ts
@@ -1,11 +1,28 @@
 /// <reference types="cypress" />
 
+type ShowcaseAttributes = {
+  name: string
+  highlight?: boolean
+}
+
 declare namespace Cypress {
   interface Chainable {
+    /**
+     * Custom command to get element by data-cy values
+     * @example cy.getByDataCy('selector')
+     */
+    getByDataCy(selector: string): Chainable<JQuery<HTMLElement>>
+
     /**
      * Custom command to check banner in page
      * @example cy.shouldRenderBanner()
      */
     shouldRenderBanner(): Chainable<Element>
+
+    /**
+     * Custom command to check showcase in page
+     * @example cy.shouldRenderShowcase()
+     */
+    shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
   }
 }
