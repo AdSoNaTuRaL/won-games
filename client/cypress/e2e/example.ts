ea347f31a393237728c75dfd4adde3e1da4d6d3a
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-15 11:50:59 -0300

    change file name

--- client/cypress/e2e/example.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../support/index.d.ts" />
-
-describe('Cypress TS', () => {
-  it('should go to Google', () => {
-    cy.google()
-  })
-})
---
+++ client/cypress/e2e/home.ts
@@ -0,0 +1,8 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Home Page', () => {
+  it('should render home sections', () => {
+    // visit page
+    cy.visit('/')
+  })
+})
