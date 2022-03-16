ad608522c6fa0364298e98f7d518c7a526f0a8bc
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-16 13:20:04 -0300

    add test to reset password (fail case)

---
+++ client/cypress/e2e/reset-password.ts
@@ -0,0 +1,41 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Reset Password', () => {
+  it('should show error if passwords does not match', () => {
+    cy.visit('/reset-password?code=1324654654654564')
+
+    cy.findByPlaceholderText(/^password/i).type('123456789')
+    cy.findByPlaceholderText(/confirm password/i).type('987654321')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.findByText(/confirm password does not match/i).should('exist')
+  })
+
+  it('should show error if code is not valid', () => {
+    cy.intercept('POST', '**/auth/reset-password', (res) => {
+      res.reply({
+        status: 400,
+        body: {
+          error: 'Bad Request',
+          message: [
+            {
+              messages: [
+                {
+                  message: 'Incorrect code provided'
+                }
+              ]
+            }
+          ]
+        }
+      })
+    })
+
+    cy.visit('/reset-password?code=wrong_code')
+
+    cy.findByPlaceholderText(/^password/i).type('123456789')
+    cy.findByPlaceholderText(/confirm password/i).type('123456789')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.findByText(/incorrect code provided/i).should('exist')
+  })
+})
